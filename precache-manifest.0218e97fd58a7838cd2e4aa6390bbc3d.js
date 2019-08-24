62e75ca3bb2ac98202f24c251bc4516bb2e13b86
Kontautas Kavaliauskas <kontautas5@yahoo.com>
2019-08-24 22:03:04 +0300

    Updates

---
+++ precache-manifest.0218e97fd58a7838cd2e4aa6390bbc3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c6dbd72530c46464e17f74f3fb37d3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec8490878c7d0d602fca",
+    "url": "/monsters-rolodex/static/css/main.a276fab2.chunk.css"
+  },
+  {
+    "revision": "4ff500561cd1e96494db",
+    "url": "/monsters-rolodex/static/js/2.b36e9024.chunk.js"
+  },
+  {
+    "revision": "ec8490878c7d0d602fca",
+    "url": "/monsters-rolodex/static/js/main.522f9d18.chunk.js"
+  },
+  {
+    "revision": "79382af826ec2b085adb",
+    "url": "/monsters-rolodex/static/js/runtime~main.85b85e5b.js"
+  }
+]);
